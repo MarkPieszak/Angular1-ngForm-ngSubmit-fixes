86f1a8300f617dde888ead6363c8cc692e5144a9
Mark Pieszak <mpieszak84@gmail.com>
2016-03-31 14:16:43 -0400

    Create ngFormFixes.directive.js

---
+++ ngFormFixes.directive.js
@@ -0,0 +1,71 @@
+var module = angular.module('ngFormFixes', []);
+
+module.directive('ngForm', function ($parse, $timeout) {
+    return {
+        link: linkFunction
+    };
+
+    function linkFunction ($scope, $element, $attrs) {
+
+        var $submit_button = findSubmitButton();
+
+        // bind Enter key
+        $element.bind('keydown', function (e) {
+            var keyCode = e.keyCode || e.which;
+            if (keyCode === 13) {
+                if ($attrs.ngSubmit) {
+                    $parse($attrs.ngSubmit)($scope);
+                    e.stopPropagation();
+                } else if ($submit_button) {
+                    $submit_button.click();
+                    e.stopPropagation();
+                }
+            }
+        });
+
+        // bind Submit click to run itself or ngSubmit
+        angular.element($submit_button).bind('click', function (e) {
+            if ($attrs.ngSubmit && angular.element(this).attr('ng-click') === undefined) {
+                $parse($attrs.ngSubmit)($scope);
+                e.stopPropagation();
+                
+                $timeout(function () {
+                    $scope[$attrs.ngForm].$submitted = true;
+                    $element.addClass('ng-submitted');
+                });
+            }
+        });
+
+        function findSubmitButton () {
+            var $buttons = [$element.find('button'), $element.find('input')];
+
+            for (var i = 0; i < $buttons.length; i++) {
+                for (var n = 0; n < $buttons[i].length; n++) {
+                    var $current = $buttons[i][n];
+                    if ($current.getAttribute('type') && $current.getAttribute('type').toLowerCase() === 'submit') {
+                        return $current;
+                    }
+
+                }
+            }
+        }
+    }
+
+});
+
+module.directive('onEnter', ['$parse', function ($parse) {
+    return {
+        link: function ($scope, $element, $attrs) {
+
+            $element.bind('keyup', function (e) {
+
+                var keyCode = e.keyCode || e.which;
+
+                if (keyCode === 13 && $attrs.onEnter) {
+                    $parse($attrs.onEnter)($scope);
+                }
+
+            });
+        }
+    };
+}]);
